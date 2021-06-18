e4460e75522642290af103f4b82c2ac2d5e73c1b
Joanna Pacia <asiameiser@gmail.com>
2021-06-18 22:21:42 +0100

    Added data

--- screens/SingleImageScreen.js
+++ screens/SingleImageScreen.js
@@ -1,117 +1,138 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text, Button, View, Image} from 'react-native';
+import TrackPlayer, {
+  TrackPlayerEvents,
+  STATE_PLAYING,
+} from 'react-native-track-player';
 import {
-    StyleSheet,
-    View,
-    Text,
-    ScrollView,
-    Button,
-    SafeAreaView,
-    FlatList,
-    StatusBar,
-    Image,
-    TouchableOpacity,
-    ActivityIndicator,
-    TouchableHighlight,
-    Animated,
-} from 'react-native';
-import {
-    path,
-    not,
-    length,
-    type,
-    pipe,
-    of,
-    toPairs,
-    map,
-    apply,
-    objOf,
-    values,
-    keys,
-    addIndex,
-    take,
-    prop,
-} from 'ramda';
-import { useSelector, useDispatch } from 'react-redux';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
-import Colors from '../constants/Colors';
+  useTrackPlayerProgress,
+  useTrackPlayerEvents,
+} from 'react-native-track-player/lib/hooks';
+import Slider from '@react-native-community/slider';
+import styles from '../styles';
 
-const SingleImageScreen = ({navigation}) => {
-    return (
-        <View style={styles.root}>
-          <Text>
-            HELLO
-          </Text>
-        </View>
-    );
+// https://github.com/rohanbhatia96/musicplayer-react-native/blob/master/App.js
+
+const songDetails = {
+  id: '1',
+  url:
+    'https://audio-previews.elements.envatousercontent.com/files/103682271/preview.mp3',
+  title: 'The Greatest Song',
+  album: 'Great Album',
+  artist: 'A Great Dude',
+  artwork: 'https://picsum.photos/300',
 };
 
-const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    eventContainer: {
-        flex: 1,
-        marginLeft: 10,
-        marginRight: 10,
-        marginBottom: 10,
-        shadowColor: Colors.primaryTeal,
-        shadowOpacity: 0.36,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 120,
-        elevation: 5,
-        backgroundColor: 'white',
-    },
-    image: {
-        width: '40%',
-        height: '45%',
-    },
-    googleMapContainer: {
-        width: '100%',
-    },
-    imagePreview: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    emptyStateText: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        color: Colors.grey5,
-    },
-    emptyStateHeader: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: Colors.grey5,
-    },
-    addButtonEventContainer: {
-        marginTop: 10,
-        borderRadius: 10,
-        alignItems: 'center',
-        borderColor:  Colors.primaryTeal,
-        borderWidth: 1,
-        backgroundColor: Colors.primaryTeal,
-    },
-    addEventButton: {
-        padding: 5,
-        fontWeight: 'bold',
-        fontSize: 14,
-        color:'white',
-    },
-    iconContainer: {
-        paddingRight: 15,
-        paddingBottom: 10,
-        flexDirection: 'row',
-        alignContent: 'flex-end',
-        justifyContent: 'space-between',
-    },
-});
+const trackPlayerInit = async () => {
+  await TrackPlayer.setupPlayer();
+  TrackPlayer.updateOptions({
+    stopWithApp: true,
+    capabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_JUMP_FORWARD,
+      TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+    ],
+  });
+  await TrackPlayer.add({
+    id: songDetails.id,
+    url: songDetails.url,
+    type: 'default',
+    title: songDetails.title,
+    album: songDetails.album,
+    artist: songDetails.artist,
+    artwork: songDetails.artwork,
+  });
+  return true;
+};
+
+const SingleImageScreen = () => {
+  const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const {position, duration} = useTrackPlayerProgress(250);
+
+   useEffect(() => {
+   const startPlayer = async () => {
+      let isInit =  await trackPlayerInit();
+      setIsTrackPlayerInit(isInit);
+   }
+   startPlayer();
+ }, []);
+
+  //this hook updates the value of the slider whenever the current position of the song changes
+ useEffect(() => {
+   if (!isSeeking && position && duration) {
+     setSliderValue(position / duration);
+   }
+ }, [position, duration]);
+
+  useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], event => {
+    if (event.state === STATE_PLAYING) {
+      setIsPlaying(true);
+    } else {
+      setIsPlaying(false);
+    }
+  });
+
+  const onButtonPressed = () => {
+    if (!isPlaying) {
+      TrackPlayer.play();
+      //setIsPlaying(true);
+    } else {
+      TrackPlayer.pause();
+      //setIsPlaying(false);
+    }
+  };
+
+  const slidingStarted = () => {
+    setIsSeeking(true);
+  };
+
+  const slidingCompleted = async value => {
+    await TrackPlayer.seekTo(value * duration);
+    setSliderValue(value);
+    setIsSeeking(false);
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: songDetails.artwork,
+          }}
+          resizeMode="contain"
+          style={styles.albumImage}
+        />
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.songTitle}>{songDetails.title}</Text>
+        <Text style={styles.artist}>{songDetails.artist}</Text>
+      </View>
+      <View style={styles.controlsContainer}>
+        <Slider
+          style={styles.progressBar}
+          minimumValue={0}
+          maximumValue={1}
+          value={sliderValue}
+          minimumTrackTintColor="#111000"
+          maximumTrackTintColor="#000000"
+          onSlidingStart={slidingStarted}
+          onSlidingComplete={slidingCompleted}
+          thumbTintColor="#000"
+        />
+        <Button
+          title={isPlaying ? 'Pause' : 'Play'}
+          onPress={onButtonPressed}
+          style={styles.playButton}
+          disabled={!isTrackPlayerInit}
+          color="#000000"
+        />
+      </View>
+    </View>
+  );
+};
 
 export default SingleImageScreen;
